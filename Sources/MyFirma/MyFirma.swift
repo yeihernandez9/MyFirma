006e4705a81370e85f259580e2e2cc809faba9ae
yeison hernandez <yeison.hernandez@grupodigital.co>
2021-02-05 16:49:19 -0500

    nuevo paquete

--- Sources/MyFirma/MyFirma.swift
+++ Sources/MyFirma/MyFirma.swift
@@ -1,3 +1,18 @@
-struct MyFirma {
-    var text = "Hello, World!"
+import Foundation
+
+public struct MyFirma {
+    
+    func localizedString(_ string: String) -> String {
+        string.localized()
+    }
+    
+    func joinAttributedStrings(_ string: NSAttributedString...) -> NSAttributedString {
+        return NSAttributedString(string: string.map{ $0.string }.joined())
+    }
+}
+
+extension String{
+    public func localized(comment: String = "") -> String {
+            return NSLocalizedString(self, comment: comment)
+        }
 }
--- Tests/MyFirmaTests/MyFirmaTests.swift
+++ Tests/MyFirmaTests/MyFirmaTests.swift
@@ -3,10 +3,15 @@ import XCTest
 
 final class MyFirmaTests: XCTestCase {
     func testExample() {
+        XCTAssertEqual(MyFirma().localizedString("Hello, World!"), "Hello, World!")
+        XCTAssertEqual(MyFirma().joinAttributedStrings(
+                    NSAttributedString(string: "Hello, "),
+                    NSAttributedString(string: "World!"))
+                    .string, "Hello, World!")
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyFirma().text, "Hello, World!")
+        //XCTAssertEqual(MyFirma().text, "Hello, World!")
     }
 
     static var allTests = [
